271d8e04d0f620eaebc7baa4ded3035265c1d56f
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-05-07 23:01:10 +0800

    j结果集工具类

---
+++ src/main/java/com/zzp/util/ResultUtil.java
@@ -0,0 +1,59 @@
+package com.zzp.util;
+
+import com.zzp.pojo.Result;
+
+/**
+ * Created by zhengping.zhu
+ * on 2017/5/7.
+ * 统一返回结果集
+ */
+public class ResultUtil {
+
+    /**
+     * 返回成功，传入返回体具体出參
+     * @param object
+     * @return
+     */
+    public static Result success(Object object){
+        Result result = new Result();
+        result.setStatus(0);
+        result.setMsg("success");
+        result.setData(object);
+        return result;
+    }
+
+    /**
+     * 提供给部分不需要出參的接口
+     * @return
+     */
+    public static Result success(){
+        return success(null);
+    }
+
+    /**
+     * 自定义错误信息
+     * @param code
+     * @param msg
+     * @return
+     */
+    public static Result error(Integer code,String msg){
+        Result result = new Result();
+        result.setStatus(code);
+        result.setMsg(msg);
+        result.setData(null);
+        return result;
+    }
+
+    /**
+     * 返回异常信息，在已知的范围内
+     * @param exceptionEnum
+     * @return
+     */
+    public static Result error(ExceptionEnum exceptionEnum){
+        Result result = new Result();
+        result.setStatus(exceptionEnum.getCode());
+        result.setMsg(exceptionEnum.getMsg());
+        result.setData(null);
+        return result;
+    }
+}
