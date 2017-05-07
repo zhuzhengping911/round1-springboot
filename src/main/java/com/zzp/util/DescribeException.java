7191303890c272ab7a7f5215eb28b5f5376fae64
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-05-07 23:02:04 +0800

    错误定义

---
+++ src/main/java/com/zzp/util/DescribeException.java
@@ -0,0 +1,38 @@
+package com.zzp.util;
+
+/**
+ *  错误描述
+ * Created by zhengping.zhu
+ * on 2017/5/7.
+ */
+public class DescribeException extends RuntimeException{
+
+    private Integer code;
+
+    /**
+     * 继承exception，加入错误状态值
+     * @param exceptionEnum
+     */
+    public DescribeException(ExceptionEnum exceptionEnum) {
+        super(exceptionEnum.getMsg());
+        this.code = exceptionEnum.getCode();
+    }
+
+    /**
+     * 自定义错误信息
+     * @param message
+     * @param code
+     */
+    public DescribeException(String message, Integer code) {
+        super(message);
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
