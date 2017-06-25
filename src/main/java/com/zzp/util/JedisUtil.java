095961c34630f1adced72435e364bdb00fff9b9b
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-06-25 23:38:52 +0800

    log

--- src/main/java/com/zzp/util/JedisUtil.java
+++ src/main/java/com/zzp/util/JedisUtil.java
@@ -1,7 +1,12 @@
 package com.zzp.util;
 
+
+import org.apache.log4j.Logger;
+
 /**
  * Created by zhuzhengping on 2017/6/25.
  */
 public class JedisUtil {
+
+    private static final Logger log = Logger.getLogger(JedisUtil.class);
 }
