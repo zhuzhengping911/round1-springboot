a3a0cbfbf4580c0c2205cdf3c1fe64650f527d35
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-07-21 00:50:17 +0800

    add

--- src/main/java/com/zzp/controller/ResultController.java
+++ src/main/java/com/zzp/controller/ResultController.java
@@ -34,10 +34,12 @@ public class ResultController {
             }else{
 
                 int i = 1/0;
-                
+
             }
 
+
         }catch (Exception e){
+
             result =  exceptionHandle.exceptionGet(e);
         }
 
