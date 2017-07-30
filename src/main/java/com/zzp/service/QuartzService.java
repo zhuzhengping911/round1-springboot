99033c9674615b5f854e1df953403f2d1a76917a
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-07-31 00:18:23 +0800

    add

--- src/main/java/com/zzp/service/QuartzService.java
+++ src/main/java/com/zzp/service/QuartzService.java
@@ -13,27 +13,28 @@ import java.util.Random;
 @Component
 public class QuartzService {
 
+//    每分钟启动
     @Scheduled(cron = "0 0/1 * * * ?")
     public void timerToNow(){
         System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
-
+//    项目运行后的每5秒一次
     @Scheduled(fixedRate = 5000)
     public void timerToZZP(){
-        System.out.println("ZZP:" + new Random().longs());
+        System.out.println("ZZP:" + new Random().nextLong() + new SimpleDateFormat("HH:mm:ss").format(new Date()));
     }
-
-    @Scheduled(fixedDelay = 5000)
+//    项目启动后每50秒启动
+    @Scheduled(fixedDelay = 50000)
     public void timerToReportCount(){
-        for (int i = 0; i < 10000; i++){
-            System.out.println("<================its" + i + "count===============>");
+        for (int i = 0; i < 10; i++){
+            System.out.println("<================its" + i + "count===============>" + new SimpleDateFormat("HH:mm:ss").format(new Date()));
         }
     }
-
-    @Scheduled(fixedDelay = 5000)
-    public void timerToReportCount(){
-        for (int i = 0; i < 10000; i++){
-            System.out.println("<================its" + i + "count===============>");
+//    第一次启动在项目启动后延迟50秒开始，之后每6秒执行一次
+    @Scheduled(initialDelay = 50000,fixedRate = 6000)
+    public void timerToReport(){
+        for (int i = 0; i < 10; i++){
+            System.out.println("<================delay :" + i + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "count===============>");
         }
     }
 }
