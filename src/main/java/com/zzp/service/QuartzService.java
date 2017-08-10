1cd2b449992077c6c0be18ef6a15796c70dd55ac
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-08-11 00:03:53 +0800

    add

--- src/main/java/com/zzp/service/QuartzService.java
+++ src/main/java/com/zzp/service/QuartzService.java
@@ -16,7 +16,7 @@ public class QuartzService {
 //    每分钟启动
     @Scheduled(cron = "0 0/1 * * * ?")
     public void timerToNow(){
-        
+
         System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
 
     }
